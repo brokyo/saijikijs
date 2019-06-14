7514596357aed923bad0cbb96f75b6dfaa2e7f60
brokyo <alexcarusillo@gmail.com>
2019-06-13 23:12:13 -0400

    alpha version

---
+++ README.md
@@ -0,0 +1,80 @@
+# Saijiki.js
+> A *Saijiki* is a resource for poets that lists *kigo*, seasonal ideas
+> pervasive in Japanese culture, for the use of poets. 
+> 
+Saijiki.js is an English language Saijiki 【歳時記】implemented in JavaScript for computational poets and net artists. It contains much of the information a traditional *Saijiki* does and has some additional parameters that enable cool digital stuff.
+
+I hope it can be used as a springboard for digital art as a traditional *Saijiki* is used for analog art.
+
+Want to know more? Check out the full introduction.
+
+
+
+## Usage
+
+Install it
+```
+npm install saijiki.js
+```
+
+Include it
+
+```
+var saijiki = require(`saijikijs`)
+```
+Filter *kigo*
+```
+// kigoList will be an array of all kigo that match your terms
+let kigoList = saijiki.filter({season: [`spring`], weather: [`clear`]})
+```
+Or get *kigo* relevant to today
+```
+// kigoList will be an array of kigo that are relevant to today's date
+// pass `true` as a param to `today()` to get kigo matching the subseason
+// you could do this buy also filter on weather and conditions. Wouldn't that be fun.
+let kigoList = saijiki.today()
+```
+
+## Things You Can Filter On
+You can filter an arbitrary number of keys and values with `filter()`
+Pass an object to `filter()` and pass the values as an array of strings
+|Key|Value  |
+|--|--|
+| season |spring, summer, autumn, winter, new year |
+| subSeason | early spring, mid spring, late spring, all spring, early summer, mid summer, late summer, all summer, early autumn, mid autumn, late autumn, all autumn, early winter, mid winter, late winter, all winter, new year |
+|category | the season, the heavens, the earth, humanity, observances, animals, plants|
+| timeOfDay | morning, afternoon, evening |
+| weather | rain, snow, cloudy, clear|
+| conditions | cold, hot
+
+## Kigo Object
+Returned *kigo* objects give you a bunch of information. Here's a sample for 日永 which I think is nice imagery
+
+```
+{
+    "pronunciation": "hinaga",
+    "hiragana": "ひなが",
+    "japanese": "日永",
+    "description": "day(s) lengthening",
+    "season": "spring",
+    "subSeason": "all spring",
+    "category": "the season",
+    "commentary": "",
+    "timeOfDay": "afternoon",
+    "weather": "",
+    "generalConditions": "",
+    "source": "http://jti.lib.virginia.edu/japanese/haiku/saijiki/1sp.html",
+    "text": "Nyūmon Saijiki"
+  }
+   ```
+
+## Going Forward
+This is an early version of what may become a full digital *saijiki*. I'd like to add more kigo, more filter categories, and links to haiku using the terms. Always interested in help or feedback.  
+
+## License
+
+```
+This is free and unencumbered software released into the public domain.
+
+For more information, please refer to <http://unlicense.org/>
+```
---
+++ index.js
@@ -0,0 +1,176 @@
+const DICT = require('./kigo.json')
+var moment = require('moment');
+
+var currentYear = moment().year()
+
+/** 
+* Saijiki break the calendar into five seasons (spring, summer, autumn, winter and new year)
+* that correspond to traditional western solar calendar dates. This table is used
+* in that conversion
+**/
+
+const DATE_TABLE = {
+	season: [
+		{
+			name: 'new year',
+			min: String(currentYear) + '-01-01',
+			max: String(currentYear) + '-01-15'
+		},
+		{
+			name: 'spring',
+			min: String(currentYear) + '-02-04',
+			max: String(currentYear) + '-05-05'
+		},
+		{
+			name: 'summer',
+			min: String(currentYear) + '-05-06',
+			max: String(currentYear) + '-08-07'
+		},
+		{
+			name: 'autumn',
+			min: String(currentYear) + '-08-08',
+			max: String(currentYear) + '-11-06'
+		},
+		{
+			name: 'winter',
+			min: String(currentYear) + '-11-07',
+			max: String(currentYear + 1) + '-02-03'
+		}
+	],
+	subSeason: [
+		{
+			name: 'new year',
+			min: String(currentYear) + '-01-01',
+			max: String(currentYear) + '-01-15'
+		},
+		{
+			name: 'early spring',
+			min: String(currentYear) + '-02-04',
+			max: String(currentYear) + '-03-05'
+		},
+		{
+			name: 'mid spring',
+			min: String(currentYear) + '-03-06',
+			max: String(currentYear) + '-04-04'
+		},
+		{
+			name: 'late spring',
+			min: String(currentYear) + '-04-05',
+			max: String(currentYear) + '-05-05'
+		},
+		{
+			name: 'early summer',
+			min: String(currentYear) + '-05-06',
+			max: String(currentYear) + '-06-05'
+		},
+		{
+			name: 'mid summer',
+			min: String(currentYear) + '-06-06',
+			max: String(currentYear) + '-07-06'
+		},
+		{
+			name: 'late summer',
+			min: String(currentYear) + '-07-07',
+			max: String(currentYear) + '-08-07'
+		},
+		{
+			name: 'early autumn',
+			min: String(currentYear) + '-08-08',
+			max: String(currentYear) + '-09-07'
+		},
+		{
+			name: 'mid autumn',
+			min: String(currentYear) + '-09-08',
+			max: String(currentYear) + '-10-07'
+		},
+		{
+			name: 'late autumn',
+			min: String(currentYear) + '-10-08',
+			max: String(currentYear) + '-11-06'
+		},
+		{
+			name: 'early winter',
+			min: String(currentYear) + '-11-07',
+			max: String(currentYear) + '-12-06'
+		},
+		{
+			name: 'mid winter',
+			min: String(currentYear) + '-12-07',
+			max: String(currentYear + 1) + '-01-04'
+		},
+		{
+			name: 'late winter',
+			min: String(currentYear + 1) + '-01-05',
+			max: String(currentYear + 1) + '-02-03'
+		}
+	]	
+}
+
+
+
+var saijiki = {
+	/**
+	 * via https://gist.github.com/jherax/f11d669ba286f21b7a2dcff69621eb72
+	 * 
+	 * Filters an array of objects with multiple criteria.
+	 *
+	 * @param  {Array}  array: the array to filter
+	 * @param  {Object} filters: an object with the filter criteria as the property names
+	 * @return {Array}
+	*/
+	_multiFilter: function(array, filters) {
+	  const filterKeys = Object.keys(filters);
+	  // filters all elements passing the criteria
+	  return array.filter((item) => {
+	    // dynamically validate all filter criteria
+	    return filterKeys.every(key => {
+	      // ignores an empty filter
+	      if (!filters[key].length) return true;
+	      return filters[key].includes(item[key]);
+	    });
+	  });
+	},
+	/**
+	 * Return an array of kigo that match search terms
+	 *
+	 * @param {terms} object: an object containing the keys to filter saijiki on. Values in form of array.
+	 * @return {Array}: array of relevant kigo  
+	*/
+	filter: function(terms) {
+		return this._multiFilter(DICT, terms)
+	},
+	/**
+	 * Returns a list of kigo that match todays date
+	 *
+	 * @param {subSeason} boolean: create kigo list from subseason rather than core season. Defaults to false
+	 * @return {Array}: array of relevant kigo  
+ 	*/
+ 	today: function(subSeason = false) {
+ 		let currentSeason, kigoArray
+
+ 		if (subSeason) {
+	 		DATE_TABLE.subSeason.forEach(season => {
+	 			if(moment().isBetween(season.min, season.max)) {
+	 				console.log(season.name)
+	 				currentSeason = season.name
+	 			}
+	 		}) 			
+ 		
+			kigoArray = DICT.filter(kigo => kigo.subSeason === currentSeason);
+ 
+ 		} else { 			
+	 		DATE_TABLE.season.forEach(season => {
+	 			if(moment().isBetween(season.min, season.max)) {
+	 				currentSeason = season.name
+	 			}
+	 		}) 			
+
+			kigoArray = DICT.filter(kigo => kigo.season === currentSeason);
+
+ 		}
+		
+		return kigoArray
+ 	}
+}
+
+export default saijiki
